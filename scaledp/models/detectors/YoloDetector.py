d87df2c1967f7dde44ba0f1211fd10824a52cf07
mykola <mykola.melnyk.ml@gmail.com>
2025-08-15 16:04:33 +0200

    Updated yolo detector

--- scaledp/models/detectors/YoloDetector.py
+++ scaledp/models/detectors/YoloDetector.py
@@ -4,7 +4,9 @@ from types import MappingProxyType
 from typing import Any
 
 from huggingface_hub import hf_hub_download
+from huggingface_hub.errors import EntryNotFoundError
 from pyspark import keyword_only
+from pyspark.ml.param import Param, Params, TypeConverters
 
 from scaledp.models.detectors.BaseDetector import BaseDetector
 from scaledp.params import HasBatchSize, HasDevice
@@ -16,6 +18,13 @@ from ...enums import Device
 
 class YoloDetector(BaseDetector, HasDevice, HasBatchSize):
     _model = None
+
+    task = Param(
+        Params._dummy(),
+        "task",
+        "Yolo task type.",
+        typeConverter=TypeConverters.toString,
+    )
 
     defaultParams = MappingProxyType(
         {
@@ -31,6 +40,8 @@ class YoloDetector(BaseDetector, HasDevice, HasBatchSize):
             "pageCol": "page",
             "pathCol": "path",
             "propagateError": False,
+            "task": "detect",
+            "onlyRotated": False,
         },
     )
 
@@ -49,12 +60,17 @@ class YoloDetector(BaseDetector, HasDevice, HasBatchSize):
 
         model = params["model"]
         if not Path(model).is_file():
-            model = hf_hub_download(repo_id=model, filename="best.pt")
+            try:
+                model = hf_hub_download(repo_id=model, filename="best.pt")
+            except EntryNotFoundError:
+                model = hf_hub_download(repo_id=model, filename="model.onnx")
 
-        detector = YOLO(model)
+        detector = YOLO(model, task=params["task"])
         device = "cpu" if int(params["device"]) == Device.CPU.value else "cuda"
-
-        cls._model = detector.to(device)
+        if "onnx" not in model:
+            cls._model = detector.to(device)
+        else:
+            cls._model = detector
         return cls._model
 
     @classmethod
@@ -72,8 +88,15 @@ class YoloDetector(BaseDetector, HasDevice, HasBatchSize):
         results_final = []
         for res, (_image, image_path) in zip(results, images):
             boxes = []
-            for box in res.boxes:
-                boxes.append(Box.from_bbox(box.xyxy[0]))
+
+            if params["task"] == "obb":
+
+                for obb in res.obb:
+                    xyxyxyxy = obb.xyxyxyxy.to("cpu").numpy().astype(int)
+                    boxes.append(Box.from_polygon(xyxyxyxy.tolist()[0]))
+            else:
+                for box in res.boxes:
+                    boxes.append(Box.from_bbox(box.xyxy[0]))
             results_final.append(
                 DetectorOutput(path=image_path, type="yolo", bboxes=boxes),
             )
